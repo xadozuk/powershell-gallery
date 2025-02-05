3d59a52b0c9971daa6e42050834890545e977f32
Xadozuk <xadozuk@gmail.com>
2025-02-05 10:38:10 +0100

    Update Open-TmuxSession to use zoxide

---
+++ Productivity/Invoke-FzfZoxide.ps1
@@ -0,0 +1,29 @@
+function Invoke-FuzzyZoxide
+{
+    $ZoxideInstalled = $null -ne (Get-Command -Name zoxide -ErrorAction SilentlyContinue)
+
+    if(-not $ZoxideInstalled)
+    {
+        throw "zoxide is not installed"
+    }
+
+    $HomePath = if($IsWindows)
+    {
+        $env:USERPROFILE
+    }
+    else
+    {
+        $env:HOME
+    }
+
+    $Selected = $null
+
+    zoxide query -l |
+        ForEach-Object { $_ -replace $HomePath, '~' } |
+        Invoke-Fzf -NoSort |
+        ForEach-Object {
+            $Selected = $_
+        }
+
+    return $Selected
+}
